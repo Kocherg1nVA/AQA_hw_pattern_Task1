1a595633899472a0a974892cec47e67eff1fba54
Vadim Kochergin <kochergin1990vadim@gmail.com>
2025-02-11 20:47:12 +0300

    DataGenerator class created

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,61 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class DataGenerator {
+    private DataGenerator(){
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        String[] cities = new String[] {"Белгород", "Брянск", "Владимир", "Воронеж", "Иваново", "Калуга", "Кострома",
+                "Курск", "Липецк", "Красногорск", "Орёл", "Рязань", "Смоленск", "Тамбов", "Тверь", "Тула", "Ярославль",
+                "Москва", "Петрозаводск", "Сыктывкар", "Архангельск", "Нарьян-Мар", "Вологда", "Калининград", "Гатчина",
+                "Мурманск", "Великий Новгород", "Псков", "Санкт-Петербург", "Майкоп", "Элиста", "Симферополь", "Краснодар",
+                "Астрахань", "Волгоград", "Ростов-на-Дону", "Севастополь", "Махачкала", "Магас", "Нальчик", "Черкесск",
+                "Владикавказ", "Грозный", "Ставрополь", "Уфа", "Йошкар-Ола", "Саранск", "Казань", "Ижевск", "Чебоксары",
+                "Пермь", "Киров", "Нижний Новгород", "Оренбург", "Пенза", "Самара", "Саратов", "Ульяновск", "Курган",
+                "Екатеринбург", "Тюмень", "Ханты-Мансийск", "Салехард", "Челябинск", "Горно-Алтайск", "Кызыл", "Абакан",
+                "Барнаул", "Красноярск", "Иркутск", "Кемерово", "Новосибирск", "Омск", "Томск", "Улан-Удэ", "Якутск",
+                "Чита", "Петропавловск-Камчатский", "Владивосток", "Хабаровск", "Благовещенск", "Магадан",
+                "Южно-Сахалинск", "Биробиджан", "Анадырь", "Донецк", "Луганск", "Мелитополь", "Геническ"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration(){
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
