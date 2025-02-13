ad6d2ec05dabf9268f55428a848e7af96e4f6351
Vadim Kochergin <kochergin1990vadim@gmail.com>
2025-02-13 08:32:16 +0300

    PlanDeliveryTest changed

--- src/test/java/ru/netology/test/PlanDeliveryTest.java
+++ src/test/java/ru/netology/test/PlanDeliveryTest.java
@@ -19,7 +19,7 @@ public class PlanDeliveryTest {
     }
 
     @Test
-    public void shouldSuccessfulPlanMeeting(){
+    public void shouldSuccessfulPlanMeeting(){ //visible
         $("[data-test-id='city'].input [placeholder='Город']").setValue(DataGenerator.generateCity());
         $("[data-test-id='date'] .input [placeholder='Дата встречи']").press(Keys.chord(Keys.SHIFT,
                 Keys.HOME), Keys.DELETE).setValue(DataGenerator.generateDate(5));
@@ -27,11 +27,11 @@ public class PlanDeliveryTest {
         $("[data-test-id='phone'].input [name='phone']").setValue(DataGenerator.generatePhone("#"));
         $("[data-test-id='agreement'].checkbox").click();
         $$("button.button").find(Condition.text("Запланировать")).click();
-        $$("[data-test-id='success-notification'] .notification__title")
-                .shouldHave(CollectionCondition.texts("Успешно!"));
-        $$("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(CollectionCondition.texts("Встреча успешно запланирована на "
-                        + DataGenerator.generateDate(5)));
+        $("[data-test-id='success-notification'] .notification__title")
+                .shouldHave(Condition.text("Успешно!")).shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на "
+                        + DataGenerator.generateDate(5))).shouldBe(Condition.visible);
     }
 
     @Test
@@ -43,25 +43,25 @@ public class PlanDeliveryTest {
         $("[data-test-id='phone'].input [name='phone']").setValue(DataGenerator.generatePhone("#"));
         $("[data-test-id='agreement'].checkbox").click();
         $$("button.button").find(Condition.text("Запланировать")).click();
-        $$("[data-test-id='success-notification'] .notification__title")
-                .shouldHave(CollectionCondition.texts("Успешно!"));
-        $$("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(CollectionCondition.texts("Встреча успешно запланирована на "
-                        + DataGenerator.generateDate(5)));
+        $("[data-test-id='success-notification'] .notification__title")
+                .shouldHave(Condition.text("Успешно!")).shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на "
+                        + DataGenerator.generateDate(5))).shouldBe(Condition.visible);
         //rePlan date
         $("[data-test-id='date'] .input [placeholder='Дата встречи']").press(Keys.chord(Keys.SHIFT,
                 Keys.HOME), Keys.DELETE).setValue(DataGenerator.generateDate(4));
         $$("button.button").find(Condition.text("Запланировать")).click();
-        $$("[data-test-id='replan-notification'] .notification__title")
-                .shouldHave(CollectionCondition.texts("Необходимо подтверждение"));
-        $$("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(CollectionCondition.texts("У вас уже запланирована встреча на другую дату. " +
-                        "Перепланировать?"));
+        $("[data-test-id='replan-notification'] .notification__title")
+                .shouldHave(Condition.text("Необходимо подтверждение")).shouldBe(Condition.visible);
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. " +
+                        "Перепланировать?")).shouldBe(Condition.visible);
         $$("button.button").find(Condition.text("Перепланировать")).click();
-        $$("[data-test-id='success-notification'] .notification__title")
-                .shouldHave(CollectionCondition.texts("Успешно!"));
-        $$("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(CollectionCondition.texts("Встреча успешно запланирована на "
-                        + DataGenerator.generateDate(4)));
+        $("[data-test-id='success-notification'] .notification__title")
+                .shouldHave(Condition.text("Успешно!")).shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на "
+                        + DataGenerator.generateDate(4))).shouldBe(Condition.visible);
     }
 }
